56846a6eefaa1ebb5c9e509840c1dcb945c70ad2
Sanchit Sahay <sanchitsahay.tech@gmail.com>
2026-01-01 19:45:24 +0530

    remove random files

--- example_grate/tools/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -x 
-
-WASMTIME_BACKTRACE_DETAILS=1 RUST_BACKTRACE=1 /home/lind/lind-wasm/src/wasmtime/target/release/wasmtime run --allow-precompiled --wasi threads=y --wasi preview2=n output/geteuid_grate.wasm getegid_grate.wasm ./mash.wasm -c "etest.wasm & imfs_grate.wasm runopen.wasm & getgid_grate.wasm gidtest.wasm" 
